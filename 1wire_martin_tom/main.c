4a570a78d753b9ccc492559eacd475fde5a21489
creosot <creosot@yandex.ru>
2017-03-24 14:13:21 +0300

    1wire_job_328p

--- 1wire_martin_tom/main.c
+++ 1wire_martin_tom/main.c
@@ -53,20 +53,205 @@
 #include "onewire.h"
 #include "ds18x20.h"
 
-#define BAUD 19200
+#define BAUD 9600
 // 2400 for 1MHz and 2MHz internal RC
 // #define BAUD 2400
 
-#define MAXSENSORS 5
-
+#define MAXSENSORS 1
 #define NEWLINESTR "\r\n"
+
+uint8_t gSensorIDs[MAXSENSORS][OW_ROMCODE_SIZE];
+
+static uint8_t search_sensors(void)
+{
+	uint8_t i;
+	uint8_t id[OW_ROMCODE_SIZE];
+	uint8_t diff, nSensors;
+	
+	uart_puts_P( NEWLINESTR "Scanning Bus for DS18X20" NEWLINESTR );
+	
+	ow_reset();
+
+	nSensors = 0;
+	
+	diff = OW_SEARCH_FIRST;
+	while ( diff != OW_LAST_DEVICE && nSensors < MAXSENSORS ) {
+		DS18X20_find_sensor( &diff, &id[0] );
+		
+		if( diff == OW_PRESENCE_ERR ) {
+			uart_puts_P( "No Sensor found" NEWLINESTR );
+			break;
+		}
+		
+		if( diff == OW_DATA_ERR ) {
+			uart_puts_P( "Bus Error" NEWLINESTR );
+			break;
+		}
+		
+		for ( i=0; i < OW_ROMCODE_SIZE; i++ )
+		gSensorIDs[nSensors][i] = id[i];
+		
+		nSensors++;
+	}
+	
+	return nSensors;
+}
+
+static void uart_put_temp(int16_t decicelsius)
+{
+// 	uart_putc(decicelsius>>8);
+// 	uart_putc('1');
+// 	uart_putc(decicelsius&0x00FF);
+// 	uart_putc('2');
+	
+	char s[10];
+
+	uart_put_int( decicelsius );
+	uart_puts_P(" deci°C, ");
+	DS18X20_format_from_decicelsius( decicelsius, s, 10 );
+	uart_puts( s );
+	uart_puts_P(" °C");
+}
+
+#if DS18X20_EEPROMSUPPORT
+
+static void th_tl_dump(uint8_t *sp)
+{
+	DS18X20_read_scratchpad( &gSensorIDs[0][0], sp, DS18X20_SP_SIZE );
+	uart_puts_P( "TH/TL in scratchpad of sensor 1 now : " );
+	uart_put_int( sp[DS18X20_TH_REG] );
+	uart_puts_P( " / " );
+	uart_put_int( sp[DS18X20_TL_REG] );
+	uart_puts_P( NEWLINESTR );
+}
+
+static void eeprom_test(void)
+{
+	uint8_t sp[DS18X20_SP_SIZE], th, tl;
+	
+	uart_puts_P( NEWLINESTR "DS18x20 EEPROM support test for first sensor" NEWLINESTR );
+	// DS18X20_eeprom_to_scratchpad(&gSensorIDs[0][0]); // already done at power-on
+	th_tl_dump( sp );
+	th = sp[DS18X20_TH_REG];
+	tl = sp[DS18X20_TL_REG];
+	tl++;
+	th++;
+	DS18X20_write_scratchpad( &gSensorIDs[0][0], th, tl, DS18B20_12_BIT );
+	uart_puts_P( "TH+1 and TL+1 written to scratchpad" NEWLINESTR );
+	th_tl_dump( sp );
+	DS18X20_scratchpad_to_eeprom( DS18X20_POWER_PARASITE, &gSensorIDs[0][0] );
+	uart_puts_P( "scratchpad copied to DS18x20 EEPROM" NEWLINESTR );
+	DS18X20_write_scratchpad( &gSensorIDs[0][0], 0, 0, DS18B20_12_BIT );
+	uart_puts_P( "TH and TL in scratchpad set to 0" NEWLINESTR );
+	th_tl_dump( sp );
+	DS18X20_eeprom_to_scratchpad(&gSensorIDs[0][0]);
+	uart_puts_P( "DS18x20 EEPROM copied back to scratchpad" NEWLINESTR );
+	DS18X20_read_scratchpad( &gSensorIDs[0][0], sp, DS18X20_SP_SIZE );
+	if ( ( th == sp[DS18X20_TH_REG] ) && ( tl == sp[DS18X20_TL_REG] ) ) {
+		uart_puts_P( "TH and TL verified" NEWLINESTR );
+		} else {
+		uart_puts_P( "verify failed" NEWLINESTR );
+	}
+	th_tl_dump( sp );
+}
+#endif /* DS18X20_EEPROMSUPPORT */
 
 
 int main(void)
 {
-    /* Replace with your application code */
+		uint8_t nSensors, i;
+		int16_t decicelsius;
+		uint8_t error;
+		
+		uart_init((UART_BAUD_SELECT((BAUD),F_CPU)));
+		
+		#ifndef OW_ONE_BUS
+		ow_set_bus(&PIND,&PORTD,&DDRD,PORTD7);
+		#endif
+		
+		sei();
+		
+		uart_puts_P( NEWLINESTR "DS18X20 1-Wire-Reader Demo by Martin Thomas" NEWLINESTR );
+		uart_puts_P(            "-------------------------------------------" );
+		
+		nSensors = search_sensors();
+		uart_put_int( (int)nSensors );
+		uart_puts_P( " DS18X20 Sensor(s) available:" NEWLINESTR );
+		
+		#if DS18X20_VERBOSE
+		for (i = 0; i < nSensors; i++ ) {
+			uart_puts_P("# in Bus :");
+			uart_put_int( (int)i + 1);
+			uart_puts_P(" : ");
+			DS18X20_show_id_uart( &gSensorIDs[i][0], OW_ROMCODE_SIZE );
+			uart_puts_P( NEWLINESTR );
+		}
+		#endif
+		
+		for ( i = 0; i < nSensors; i++ ) {
+			uart_puts_P( "Sensor# " );
+			uart_put_int( (int)i+1 );
+			uart_puts_P( " is a " );
+			if ( gSensorIDs[i][0] == DS18S20_FAMILY_CODE ) {
+				uart_puts_P( "DS18S20/DS1820" );
+				} else if ( gSensorIDs[i][0] == DS1822_FAMILY_CODE ) {
+				uart_puts_P( "DS1822" );
+			}
+			else {
+				uart_puts_P( "DS18B20" );
+			}
+			uart_puts_P( " which is " );
+			if ( DS18X20_get_power_status( &gSensorIDs[i][0] ) == DS18X20_POWER_PARASITE ) {
+				uart_puts_P( "parasite" );
+				} else {
+				uart_puts_P( "externally" );
+			}
+			uart_puts_P( " powered" NEWLINESTR );
+		}	
+		
+// 		#if DS18X20_EEPROMSUPPORT
+// 		if ( nSensors > 0 ) {
+// 			eeprom_test();
+// 		}
+// 		#endif	
+		
+		if ( nSensors == 1 ) {
+			uart_puts_P( NEWLINESTR "There is only one sensor "
+			"-> Demo of \"DS18X20_read_decicelsius_single\":" NEWLINESTR );
+			i = gSensorIDs[0][0]; // family-code for conversion-routine
+			/*DS18X20_start_meas( DS18X20_POWER_PARASITE, NULL );*/
+			DS18X20_start_meas( DS18X20_POWER_EXTERN, NULL ); //start measure
+			_delay_ms( DS18B20_TCONV_12BIT ); //delay for measure
+			DS18X20_read_decicelsius_single( i, &decicelsius );
+			if (decicelsius>230)
+			{
+				uart_puts_P( NEWLINESTR "temp>230");
+			} 
+			if (decicelsius>235)
+			{
+				uart_puts_P( NEWLINESTR "temp>235");
+			}
+			if (decicelsius>240)
+			{
+				uart_puts_P( NEWLINESTR "temp>240");
+			}
+			if (decicelsius>245)
+			{
+				uart_puts_P( NEWLINESTR "temp>245");
+			}
+			uart_puts_P( NEWLINESTR );
+			uart_put_temp( decicelsius );
+			uart_puts_P( NEWLINESTR );
+		}	
+
     while (1) 
     {
+		DDRB |= 0b00111111;
+		PORTB |= 0b00111111;
+		DDRD |= 0b01100000;
+		PORTD |= 0b01100000;
+		DDRC |= 0b00001111;
+		PORTC |= 0b00001111;
     }
 }
 
